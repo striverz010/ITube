66c8ab255618c1eb1754862b194c9848cd3aa139
striverz010 <striveredu010@gmail.com>
2024-01-21 14:50:47 +0530

    new elements added

---
+++ src/components/VideoCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const VideoCard = ({ info }) => {
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+  return <div className="p-2 m-2 w-60 shadow-lg">
+    <img className="rounded-lg" alt="thumbnails" src={thumbnails.medium.url}/>
+    <ul>
+        <li className="font-bold py-2">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} Million views</li>
+    </ul>
+  </div>;
+};
+
+export default VideoCard;
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const GOOGLE_API_KEY = "AIzaSyAIWnqZCBkhdUDqHs47v-6tBRmGWDQBu3k";
+
+export const OFFSET_LIVE_CHAT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+

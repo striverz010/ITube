3341b50c02a5c3dad21bf9cac2fb6285f182e68c
striverz010 <striveredu010@gmail.com>
2024-03-28 10:28:20 +0530

    the space is added

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,8 @@ import Sidebar from "./Sidebar";
 import MainContainer from "./MainContainer";
 import { Outlet } from "react-router-dom";
 
+
+
 const Body = () => {
   return (
     <div className="flex">
